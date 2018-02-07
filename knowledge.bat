741c52b40bd77d840ec16b10d6de209175b6e1ab
jeffcheng <b98502008@gmail.com>
2018-02-07 16:50:53 +0800

    Update knowledge.bat

--- knowledge.bat
+++ knowledge.bat
@@ -1,6 +1,27 @@
 @echo off
 
 ::==============================================
+:: Change Code Page
+
+chcp Code_page
+
+:: Code page	Country/ Region/ Language
+:: 437		United States
+:: 850		Multilingual (Latin I)
+:: 852		Slavic (Latin II)
+:: 855		Cyrillic (Russian)
+:: 857		Turkish
+:: 860		Portuguese
+:: 861		Icelandic
+:: 863		Canadian-French
+:: 865		Nordic
+:: 866		Russian
+:: 869		Modern Greek
+:: 1252		West European Latin
+:: 65000	UTF-7 *
+:: 65001	UTF-8 *
+
+::==============================================
 :: To get the list of local users on the computer
 net users
 
